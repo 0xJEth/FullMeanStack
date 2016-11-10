661d340d0721a6649db6557e62d939b9a673988c
jamesmurphy0622 <james.murphy0622@gmail.com>
2016-11-10 14:55:07 -0700

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,7 +14,7 @@ angular.module('comment', [])
     $scope.addComment = function() {
       var newcomment = {title:$scope.formContent,upvotes:0};
       $scope.formContent='';
-      $http.post('/comments', comment).success(function(data){
+      $http.post('/comments', newcomment).success(function(data){
         $scope.comments.push(data);
       });
     };
