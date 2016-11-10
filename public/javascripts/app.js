cc626867c2c70c5a19aaced3c6a584b303b7e7cc
jamesmurphy0622 <james.murphy0622@gmail.com>
2016-11-10 15:11:43 -0700

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,15 +2,6 @@ angular.module('comment', [])
 .controller('MainCtrl', [
   '$scope','$http',
   function($scope,$http){
-    $scope.test = 'Hello world!';
-	$scope.comments = [
-      {title:'Comment 1', upvotes:5},
-      {title:'Comment 2', upvotes:6},
-      {title:'Comment 3', upvotes:1},
-      {title:'Comment 4', upvotes:4},
-      {title:'Comment 5', upvotes:3}
-    ];
-    
     $scope.addComment = function() {
       var newcomment = {title:$scope.formContent,upvotes:0};
       $scope.formContent='';
