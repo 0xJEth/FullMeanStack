bcb69483015d9d72748af4bb1678109469089b11
jamesmurphy0622 <james.murphy0622@gmail.com>
2018-11-14 01:32:47 -0700

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,31 +1,35 @@
 angular.module('comment', [])
-.controller('MainCtrl', [
-  '$scope','$http',
-  function($scope,$http){
-    $scope.comments = [];
-    $scope.addComment = function() {
-      var newcomment = {title:$scope.formContent,upvotes:0};
-      $scope.formContent='';
-      $http.post('/comments', newcomment).success(function(data){
-        $scope.comments.push(data);
-      });
-    };
-    $scope.upvote = function(comment) {
-      return $http.put('/comments/' + comment._id + '/upvote')
-        .success(function(data){
-          console.log("upvote worked");
-          comment.upvotes = data.upvotes;
-        });
-    };
-	$scope.incrementUpvotes = function(comment) {
-	  $scope.upvote(comment);
-    };
-    $scope.getAll = function() {
-      return $http.get('/comments').success(function(data){
-        angular.copy(data, $scope.comments);
-      });
-    };
-    $scope.getAll();
+    .controller('MainCtrl', [
+        '$scope', '$http',
+        function($scope, $http) {
+            $scope.comments = [];
+            $scope.addComment = function() {
+                var newcomment = { title: $scope.formContent, upvotes: 0 };
+                $http.post('/comments', newcomment).success(function(data) {
+                    $scope.comments.push(data);
+                });
+                $scope.formContent = '';
+            };
+            $scope.incrementUpvotes = function(comment) {
+                $http.put('/comments/' + comment._id + '/upvote')
+                    .success(function(data) {
+                        console.log("upvote worked");
+                        comment.upvotes += 1;
+                    });
+            };
+            $scope.delete = function(comment) {
+                $http.delete('/comments/' + comment._id)
+                    .success(function(data) {
+                        console.log("delete worked");
+                    });
+                $scope.getAll();
+            };
+            $scope.getAll = function() {
+                return $http.get('/comments').success(function(data) {
+                    angular.copy(data, $scope.comments);
+                });
+            };
+            $scope.getAll();
+        }
+    ]);
 
-  }
-]);
